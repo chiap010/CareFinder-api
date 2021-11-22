71f3c798da45243564f11785c6da1db0a9414c55
chiap010 <78487437+chiap010@users.noreply.github.com>
2021-11-22 13:35:04 -0600

    More comments

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -7,62 +7,94 @@ const apikey = require("apikeygen").apikey;
 const resources = require("../helpers/resources");
 const { httpCodeBadRequest } = require("../helpers/resources");
 
+// Handle User HTTP GET
 exports.read = async (req, res) => {
+       // Check to see if the API key is valid and grab the permission level
        let isValidKey = await isValidAPIKey(req);
        let permissionLevel = await getPermissionLevel(req);
 
+       // If the API key is valid and the user's permissions are admin, then we can return
+       // API user data
        if (isValidKey && permissionLevel === "ADMIN") {
+              // When there is nothing in the query string...
               if (Object.keys(req.query).length === 0) {
+                     // Grab the User collection using find() with no object passed into it.
                      const users = await User.find().exec();
 
+                     // If Users were returned from Mongo DB, we want to return that in a JSON list
                      if (users && users.length > 0) {
                             res.status(resources.httpCodeOK).json({
                                    data: users,
                             });
-                     } else if (users && users.length === 0) {
+                     }
+                     // If there are no User documents inside the collection, we should return
+                     // that we could find find any resources.
+                     else if (users && users.length === 0) {
                             res.status(resources.httpCodeNotFound).json({
                                    error: resources.httpStringNotFound,
                             });
-                     } else {
+                     }
+                     // Otherwise, just return a bad request
+                     else {
                             res.status(resources.httpCodeBadRequest).json({
                                    error: resources.httpStringBadRequest,
                             });
                      }
-              } else if (req.query.key) {
+              }
+
+              // If the query string isn't blank, and the user has passed in an API key
+              // via the query string, we'll use that to check Users in Mongo where the
+              // API key passed in matches a User in the collection.
+              else if (req.query.key) {
                      const users = await User.find({
                             api_key: req.query.key,
                      }).exec();
 
+                     // When we have a result, return that JSON back
                      if (users && users.length > 0) {
                             res.status(resources.httpCodeOK).json({
                                    data: users,
                             });
-                     } else if (users && users.length === 0) {
+                     }
+                     // If nothing was returned back from Mongo, return that we couldn't
+                     // find any resources.
+                     else if (users && users.length === 0) {
                             res.status(resources.httpCodeNotFound).json({
                                    error: resources.httpStringNotFound,
                             });
-                     } else {
+                     }
+                     // Otherwise, return a bad request
+                     else {
                             res.status(resources.httpCodeBadRequest).json({
                                    error: resources.httpStringBadRequest,
                             });
                      }
-              } else {
+              }
+              // If we got something else in the query string -- if it's not nothing or not an
+              // API key being passed in, return a bad request
+              else {
                      res.status(resources.httpCodeBadRequest).json({
                             error: resources.httpStringBadRequest,
                      });
               }
-       } else {
+       }
+       // If the user wasn't authenticated or doesn't have the rights to view User API documents,
+       // return that they aren't authorized to do this operation.
+       else {
               res.status(resources.httpCodeUnauthorized).json({
                      error: resources.httpStringUnauthorized,
               });
        }
 };
 
+// Handle User HTTP POST
 exports.post = async (req, res) => {
        let isValidKey = await isValidAPIKey(req);
        // let permissionLevel = await getPermissionLevel(req);
 
+       // TODO:  read POST form HTTP body.  Hardcoding for now
        if (isValidKey) {
+              // If the query string is empty, allow post
               if (Object.keys(req.query).length === 0) {
                      const apiKeyToUse = apikey();
                      const post = new User({
@@ -71,6 +103,8 @@ exports.post = async (req, res) => {
                             permission: "ADMIN",
                             createdTs: Date.now,
                      });
+
+                     // Attempt to save our new User document
                      try {
                             const savedPost = await post.save();
                             res.json({ data: savedPost });
@@ -78,19 +112,28 @@ exports.post = async (req, res) => {
                             res.json({ error: err });
                             console.log(err);
                      }
-              } else {
+              }
+
+              // If the query string isn't empty, I'm not really sure how to handle that, so
+              // just return a bad request
+              else {
                      res.status(resources.httpCodeBadRequest).json({
                             error: resources.httpStringBadRequest,
                      });
               }
-       } else {
+       }
+       // If the API key wasn't valid, don't let the user do anything return that
+       // they aren't authorized.
+       else {
               res.status(resources.httpCodeUnauthorized).json({
                      error: resources.httpStringUnauthorized,
               });
        }
 };
 
+// Handle User HTTP DELETE
 exports.remove = async (req, res) => {
+       // Check to see if the API key is valid and grab the permission level
        let isValidKey = await isValidAPIKey(req);
        let permissionLevel = await getPermissionLevel(req);
 
